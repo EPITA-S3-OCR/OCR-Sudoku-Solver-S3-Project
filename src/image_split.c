46d5c33704fb360eb063665d82125e69e3475d76
CapnDev <53230147+CapnDev-Git@users.noreply.github.com>
2022-10-05 22:43:29 +0200

    Updated to support hexa-sudokus
    Added a parameter 'sides' to the split_image function.

--- src/image_split.c
+++ src/image_split.c
@@ -5,22 +5,22 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-void splitImage(SDL_Surface* sfcSudoku, int orgX, int orgY, int w, int h)
+void split_image(SDL_Surface* sfcSudoku, int orgX, int orgY, int w, int h, int sides)
 {
 	// Define the size of each sudoku tile
-	int tileX = w/9, tileY = h/9;
+	int tileX = w/sides, tileY = h/sides;
 
 	// Loop through each sudoku tiles
-	for (int i = 0; i < 9; i++)
+	for (int i = 0; i < sides; i++)
 	{
-		for (int j = 0; j < 9; j++)
+		for (int j = 0; j < sides; j++)
 		{
 			// Create a new surface for the current tile
 			SDL_Surface* sfcTile = SDL_CreateRGBSurface(0, tileX, tileY, 32, 0, 0, 0, 0);
 
 			// Create the rectangle at the correct coordinates
 			SDL_Rect currTile;
-			int threshold = 5;
+			int threshold = 0; // to set if needed, debug feature mostly
 			currTile.x = j*tileX + orgX + threshold;
 			currTile.y = i*tileY + orgY;
 			currTile.w = tileX;
@@ -57,8 +57,8 @@ int main(int argc, char** argv)
 	//int ORGX = 342, ORGY = 216, W = 830, H = 821;		// sudoku2
 	//int ORGX = 130, ORGY = 89, W = 520, H = 520;		// sudoku3
 	//int ORGX = 415, ORGY = 162, W = 1132, H = 1147;	// sudoku4
-	splitImage(surface, ORGX, ORGY, W, H);
-
+	split_image(surface, ORGX, ORGY, W, H, 9); // can be changed to 16 for hexadoku
+	
 	// Free the main surface
 	SDL_FreeSurface(surface);
 
