b92cf52fa24b594b618293f090b120e8b921ac92
David Calderon <92121401+Davidca089@users.noreply.github.com>
2022-10-15 13:32:23 +0200

    I am getting a stack smashing error, but it works

---
+++ solver.h
@@ -0,0 +1,11 @@
+#pragma once
+// Reads sudoku from file
+#include "stddef.h"
+
+// File handling stuff
+char* loadSudoku(char sudoku[9][9], char path[], size_t s);
+int createFile(char grid[9][9], char* path);
+
+//Solving sudoku algortihms.
+int Solve(char grid[9][9]);
+int possiblePlacement(int row, int col, char num, char grid[9][9]);
