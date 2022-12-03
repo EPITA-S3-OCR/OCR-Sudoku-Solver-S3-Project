60cafe2737af737efcb78810053be0d4856dac80
David <david-ortiz.calderon@epita.fr>
2022-12-03 18:16:25 +0100

    added some css for the bg and buttons

--- src/user-interface/main.c
+++ src/user-interface/main.c
@@ -1,10 +1,29 @@
 #include <gtk/gtk.h>
 
+void myCSS(void){
+    GtkCssProvider *provider;
+    GdkDisplay *display;
+    GdkScreen *screen;
+
+    provider = gtk_css_provider_new ();
+    display = gdk_display_get_default ();
+    screen = gdk_display_get_default_screen (display);
+    gtk_style_context_add_provider_for_screen (screen, GTK_STYLE_PROVIDER (provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+    const gchar *myCssFile = "mystyle.css";
+    GError *error = 0;
+
+    gtk_css_provider_load_from_file(provider, g_file_new_for_path(myCssFile), &error);
+    g_object_unref (provider);
+}
+
 
 int main (int argc, char *argv[])
 {
     // Initializes GTK.
     gtk_init(NULL, NULL);
+
+    myCSS();
 
     // Loads the UI description and builds the UI.
     // (Exits if an error occurs.)
@@ -21,12 +40,18 @@ int main (int argc, char *argv[])
     GtkWindow* window = GTK_WINDOW(gtk_builder_get_object(builder, "window"));
     GtkButton* start_button = GTK_BUTTON(gtk_builder_get_object(builder, "button-one"));
 
+    gtk_widget_set_name(GTK_WIDGET(window), "backGround");
+
     // Connects signal handlers.
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     //g_signal_connect(start_button, "clicked", G_CALLBACK(on_start), NULL);
+    //
+
+    
 
 
-    gtk_widget_show(window);
+    
+    gtk_widget_show(GTK_WIDGET(window));
 
     // Runs the main loop.
     gtk_main();
